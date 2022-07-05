6255c0b65d9368bb5c2208d98748fde06addfd89
Ori Shavit <shavitori1@gmail.com>
2022-07-05 09:15:13 +0300

    Update SQL Server Review.sql

--- SQL Server Review.sql
+++ SQL Server Review.sql
@@ -163,6 +163,8 @@ group by
 	,CASE WHEN database_name = 'tempdb' THEN 'tempdb' + ' ' + file_type 
 		ELSE 'userdb' + ' ' + file_type END
 
+SET @checkpointprint = 'Finished SQL File Info | Total Duration(ms): '+CAST(DATEDIFF(MILLISECOND,@starttime,GETDATE()) AS nvarchar)
+RAISERROR (@checkpointprint,0,1) WITH NOWAIT	
 
 /*
 Global Traces 
@@ -247,8 +249,10 @@ OPEN db_config
 	END
 CLOSE db_config
 DEALLOCATE db_config
-
 SELECT 'Advanced DB Option' as title,* FROM @advanceDB
+
+SET @checkpointprint = 'Finished Advanced DB Option | Total Duration(ms): '+CAST(DATEDIFF(MILLISECOND,@starttime,GETDATE()) AS nvarchar)
+RAISERROR (@checkpointprint,0,1) WITH NOWAIT	
  
  
 /* DB File growth */
@@ -313,94 +317,98 @@ SELECT
 from msdb.dbo.log_shipping_primary_databases db1
 
 
+
+
 IF @ShowJobSchedules > 0 BEGIN
 /* Job schedules originally copied from dba.stackexchange.com/questions/148321 */
 	select 'Job Schedules' AS 'title'
-       ,S.name AS jobname
-	   ,S.enabled AS job_enabled
-	   ,ISNULL(SS.enabled,0) AS schedule_enabled
-	   ,ISNULL(SS.name,'Not found') AS schedule_name,
-       CASE(SS.freq_type)
-            WHEN 1  THEN 'Once'
-            WHEN 4  THEN 'Daily'
-            WHEN 8  THEN (case when (SS.freq_recurrence_factor > 1) then  'Every ' + convert(varchar(3),SS.freq_recurrence_factor) + ' Weeks'  else 'Weekly'  end)
-            WHEN 16 THEN (case when (SS.freq_recurrence_factor > 1) then  'Every ' + convert(varchar(3),SS.freq_recurrence_factor) + ' Months' else 'Monthly' end)
-            WHEN 32 THEN 'Every ' + convert(varchar(3),SS.freq_recurrence_factor) + ' Months' 
-            WHEN 64 THEN 'SQL Startup'
-            WHEN 128 THEN 'SQL Idle'
-            ELSE 'Not found'
-        END AS frequency,
-       ISNULL(CASE
-            WHEN (freq_type = 1)                       then 'One time only'
-            WHEN (freq_type = 4 and freq_interval = 1) then 'Every Day'
-            WHEN (freq_type = 4 and freq_interval > 1) then 'Every ' + convert(varchar(10),freq_interval) + ' Days'
-            WHEN (freq_type = 8) then (select 'Weekly Schedule' = MIN(D1+ D2+D3+D4+D5+D6+D7 )
-                                        from (select SS.schedule_id,
-                                                        freq_interval,
-                                                        'D1' = CASE WHEN (freq_interval & 1  <> 0) then 'Sun ' ELSE '' END,
-                                                        'D2' = CASE WHEN (freq_interval & 2  <> 0) then 'Mon '  ELSE '' END,
-                                                        'D3' = CASE WHEN (freq_interval & 4  <> 0) then 'Tue '  ELSE '' END,
-                                                        'D4' = CASE WHEN (freq_interval & 8  <> 0) then 'Wed '  ELSE '' END,
-                                                    'D5' = CASE WHEN (freq_interval & 16 <> 0) then 'Thu '  ELSE '' END,
-                                                        'D6' = CASE WHEN (freq_interval & 32 <> 0) then 'Fri '  ELSE '' END,
-                                                        'D7' = CASE WHEN (freq_interval & 64 <> 0) then 'Sat '  ELSE '' END
-                                                    from msdb..sysschedules ss
-                                                where freq_type = 8
-                                            ) as F
-                                        where schedule_id = SJ.schedule_id
-                                    )
-            WHEN (freq_type = 16) then 'Day ' + convert(varchar(2),freq_interval)
-            WHEN (freq_type = 32) then (select  freq_rel + WDAY
-                                        from (select SS.schedule_id,
-                                                        'freq_rel' = CASE(freq_relative_interval)
-                                                                    WHEN 1 then 'First'
-                                                                    WHEN 2 then 'Second'
-                                                                    WHEN 4 then 'Third'
-                                                                    WHEN 8 then 'Fourth'
-                                                                    WHEN 16 then 'Last'
-                                                                    ELSE '??'
-                                                                    END,
-                                                    'WDAY'     = CASE (freq_interval)
-                                                                    WHEN 1 then ' Sun'
-                                                                    WHEN 2 then ' Mon'
-                                                                    WHEN 3 then ' Tue'
-                                                                    WHEN 4 then ' Wed'
-                                                                    WHEN 5 then ' Thu'
-                                                                    WHEN 6 then ' Fri'
-                                                                    WHEN 7 then ' Sat'
-                                                                    WHEN 8 then ' Day'
-                                                                    WHEN 9 then ' Weekday'
-                                                                    WHEN 10 then ' Weekend'
-                                                                    ELSE '??'
-                                                                    END
-                                                from msdb..sysschedules SS
-                                                where SS.freq_type = 32
-                                                ) as WS 
-                                        where WS.schedule_id = SS.schedule_id
-                                        ) 
-        END,'Not found') AS interval,
-        CASE (freq_subday_type)
-            WHEN 1 then   left(stuff((stuff((replicate('0', 6 - len(active_start_time)))+ convert(varchar(6),active_start_time),3,0,':')),6,0,':'),8)
-            WHEN 2 then 'Every ' + convert(varchar(10),freq_subday_interval) + ' seconds'
-            WHEN 4 then 'Every ' + convert(varchar(10),freq_subday_interval) + ' minutes'
-            WHEN 8 then 'Every ' + convert(varchar(10),freq_subday_interval) + ' hours'
-            ELSE 'Not found'
-        END AS [time],
-        ISNULL(CASE SJ.next_run_date
-            WHEN 0 THEN cast('Not found' as char(10))
-            ELSE convert(char(10), convert(datetime, convert(char(8),SJ.next_run_date)),120)  + ' ' + left(stuff((stuff((replicate('0', 6 - len(next_run_time)))+ convert(varchar(6),next_run_time),3,0,':')),6,0,':'),8)
-        END,'Not found') AS next_runtime,
-		ISNULL(failed.cnt,0) fails_in_hist
-from msdb.dbo.sysjobs S
-left join msdb.dbo.sysjobschedules SJ on S.job_id = SJ.job_id  
-left join msdb.dbo.sysschedules SS on SS.schedule_id = SJ.schedule_id
-left join (
-	select H.job_id,count(*) cnt from msdb.dbo.sysjobhistory H
-	where H.run_status = 0
-	group by H.job_id
-) failed on failed.job_id = S.job_id
-order by jobname
+	       ,S.name AS jobname
+		   ,S.enabled AS job_enabled
+		   ,ISNULL(SS.enabled,0) AS schedule_enabled
+		   ,ISNULL(SS.name,'Not found') AS schedule_name,
+	       CASE(SS.freq_type)
+	            WHEN 1  THEN 'Once'
+	            WHEN 4  THEN 'Daily'
+	            WHEN 8  THEN (case when (SS.freq_recurrence_factor > 1) then  'Every ' + convert(varchar(3),SS.freq_recurrence_factor) + ' Weeks'  else 'Weekly'  end)
+	            WHEN 16 THEN (case when (SS.freq_recurrence_factor > 1) then  'Every ' + convert(varchar(3),SS.freq_recurrence_factor) + ' Months' else 'Monthly' end)
+	            WHEN 32 THEN 'Every ' + convert(varchar(3),SS.freq_recurrence_factor) + ' Months' 
+	            WHEN 64 THEN 'SQL Startup'
+	            WHEN 128 THEN 'SQL Idle'
+	            ELSE 'Not found'
+	        END AS frequency,
+	       ISNULL(CASE
+	            WHEN (freq_type = 1)                       then 'One time only'
+	            WHEN (freq_type = 4 and freq_interval = 1) then 'Every Day'
+	            WHEN (freq_type = 4 and freq_interval > 1) then 'Every ' + convert(varchar(10),freq_interval) + ' Days'
+	            WHEN (freq_type = 8) then (select 'Weekly Schedule' = MIN(D1+ D2+D3+D4+D5+D6+D7 )
+	                                        from (select SS.schedule_id,
+	                                                        freq_interval,
+	                                                        'D1' = CASE WHEN (freq_interval & 1  <> 0) then 'Sun ' ELSE '' END,
+	                                                        'D2' = CASE WHEN (freq_interval & 2  <> 0) then 'Mon '  ELSE '' END,
+	                                                        'D3' = CASE WHEN (freq_interval & 4  <> 0) then 'Tue '  ELSE '' END,
+	                                                        'D4' = CASE WHEN (freq_interval & 8  <> 0) then 'Wed '  ELSE '' END,
+	                                                    'D5' = CASE WHEN (freq_interval & 16 <> 0) then 'Thu '  ELSE '' END,
+	                                                        'D6' = CASE WHEN (freq_interval & 32 <> 0) then 'Fri '  ELSE '' END,
+	                                                        'D7' = CASE WHEN (freq_interval & 64 <> 0) then 'Sat '  ELSE '' END
+	                                                    from msdb..sysschedules ss
+	                                                where freq_type = 8
+	                                            ) as F
+	                                        where schedule_id = SJ.schedule_id
+	                                    )
+	            WHEN (freq_type = 16) then 'Day ' + convert(varchar(2),freq_interval)
+	            WHEN (freq_type = 32) then (select  freq_rel + WDAY
+	                                        from (select SS.schedule_id,
+	                                                        'freq_rel' = CASE(freq_relative_interval)
+	                                                                    WHEN 1 then 'First'
+	                                                                    WHEN 2 then 'Second'
+	                                                                    WHEN 4 then 'Third'
+	                                                                    WHEN 8 then 'Fourth'
+	                                                                    WHEN 16 then 'Last'
+	                                                                    ELSE '??'
+	                                                                    END,
+	                                                    'WDAY'     = CASE (freq_interval)
+	                                                                    WHEN 1 then ' Sun'
+	                                                                    WHEN 2 then ' Mon'
+	                                                                    WHEN 3 then ' Tue'
+	                                                                    WHEN 4 then ' Wed'
+	                                                                    WHEN 5 then ' Thu'
+	                                                                    WHEN 6 then ' Fri'
+	                                                                    WHEN 7 then ' Sat'
+	                                                                    WHEN 8 then ' Day'
+	                                                                    WHEN 9 then ' Weekday'
+	                                                                    WHEN 10 then ' Weekend'
+	                                                                    ELSE '??'
+	                                                                    END
+	                                                from msdb..sysschedules SS
+	                                                where SS.freq_type = 32
+	                                                ) as WS 
+	                                        where WS.schedule_id = SS.schedule_id
+	                                        ) 
+	        END,'Not found') AS interval,
+	        CASE (freq_subday_type)
+	            WHEN 1 then   left(stuff((stuff((replicate('0', 6 - len(active_start_time)))+ convert(varchar(6),active_start_time),3,0,':')),6,0,':'),8)
+	            WHEN 2 then 'Every ' + convert(varchar(10),freq_subday_interval) + ' seconds'
+	            WHEN 4 then 'Every ' + convert(varchar(10),freq_subday_interval) + ' minutes'
+	            WHEN 8 then 'Every ' + convert(varchar(10),freq_subday_interval) + ' hours'
+	            ELSE 'Not found'
+	        END AS [time],
+	        ISNULL(CASE SJ.next_run_date
+	            WHEN 0 THEN cast('Not found' as char(10))
+	            ELSE convert(char(10), convert(datetime, convert(char(8),SJ.next_run_date)),120)  + ' ' + left(stuff((stuff((replicate('0', 6 - len(next_run_time)))+ convert(varchar(6),next_run_time),3,0,':')),6,0,':'),8)
+	        END,'Not found') AS next_runtime,
+			ISNULL(failed.cnt,0) fails_in_hist
+	from msdb.dbo.sysjobs S
+	left join msdb.dbo.sysjobschedules SJ on S.job_id = SJ.job_id  
+	left join msdb.dbo.sysschedules SS on SS.schedule_id = SJ.schedule_id
+	left join (
+		select H.job_id,count(*) cnt from msdb.dbo.sysjobhistory H
+		where H.run_status = 0
+		group by H.job_id
+	) failed on failed.job_id = S.job_id
+	order by jobname
 
+	SET @checkpointprint = 'Finished Job Schedules| Total Duration(ms): '+CAST(DATEDIFF(MILLISECOND,@starttime,GETDATE()) AS nvarchar)
+	RAISERROR (@checkpointprint,0,1) WITH NOWAIT	
 END
 
 IF @checkMissingBackups > 0
@@ -452,6 +460,8 @@ BEGIN
 	;
 	INSERT INTO #warnings(category,warning,fix,note)
 	SELECT 'Missing Backups', 'Database '+ QUOTENAME(name) + ' ' + warning, 'Make backups', 'Thresholds: full: '+CAST(@bak_alert AS NVARCHAR)+'d, log: '+CAST(@trn_alert AS NVARCHAR)+'d' from #backupcheck where warning > '' 
+	SET @checkpointprint = 'Finished Missing Backups | Total Duration(ms): '+CAST(DATEDIFF(MILLISECOND,@starttime,GETDATE()) AS nvarchar)
+	RAISERROR (@checkpointprint,0,1) WITH NOWAIT	
 END
 
 IF @ShowProcStats > 0
@@ -470,6 +480,10 @@ BEGIN
 		 where PS.database_id not in (32767,1,2,3,4)
 	group by PS.object_id,PS.database_id
 	order by avg_runtime_ms desc
+
+	SET @checkpointprint = 'Finished Proc Stats | Total Duration(ms): '+CAST(DATEDIFF(MILLISECOND,@starttime,GETDATE()) AS nvarchar)
+	RAISERROR (@checkpointprint,0,1) WITH NOWAIT	
+
 END
 
 IF @showMissingIndexes > 0
